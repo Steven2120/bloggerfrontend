55ffce96ae200b8c207bede126d5e5e457b28026
Steven Moyano <stevenmoyano1@gmail.com>
2022-07-18 10:27:19 -0500

    added pages

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Routes, Route } from "react-router-dom";
 import BlogsPage from "./Pages/Blogs";
 import { useState, useEffect } from "react";
 import PostBlogPage from "./Pages/PostBlogPage";
+import BlogManager from "./Pages/BlogManager";
 
 const urlEndpoint = "http://localhost:4000";
 
@@ -16,6 +17,9 @@ function App() {
   const [limit, setLimit] = useState(Number());
   const [page, setPage] = useState(Number(1));
   const [isFetching, setIsFetching] = useState(false);
+
+  const [adminBlogList, setAdminBlogList] = useState({ message: [] });
+  const [adminBlogsLoading, setAdminBlogsLoading] = useState(false);
 
   const blogSubmit = async (blog) => {
     const url = urlEndpoint + "/blogs/blog-submit";
@@ -30,6 +34,22 @@ function App() {
     const responseJSON = await response.json();
     return responseJSON;
   };
+  const deleteBlog = async (blogId) => {
+    setAdminBlogsLoading(true);
+    const url = `${urlEndpoint}/admin/delete-blog/${blogId}`;
+    const response = await fetch(url, {
+      method: "DELETE",
+    });
+    const responseJSON = await response.json();
+    setAdminBlogsLoading(false);
+  };
+
+  const fetchSingleBlog = async (blogId) => {
+    const url = `${urlEndpoint}/blogs/single-blog/${blogId}`;
+    const response = await fetch(url);
+    const responseJSON = await response.json();
+    return responseJSON.message;
+  };
 
   useEffect(() => {
     const fetchData = async () => {
@@ -41,6 +61,16 @@ function App() {
     };
     fetchData();
   }, [sortField, sortOrder, filterField, filterValue, limit, page, isFetching]);
+
+  useEffect(() => {
+    const fetchAdminBlogList = async () => {
+      const apiResponse = await fetch(`${urlEndpoint}/admin/blog-list`);
+      const json = await apiResponse.json();
+      setAdminBlogList(json);
+      return json;
+    };
+    fetchAdminBlogList();
+  }, [adminBlogsLoading]);
 
   return (
     <div className="App">
@@ -72,6 +102,18 @@ function App() {
               <PostBlogPage
                 blogSubmit={blogSubmit}
                 setIsFetching={setIsFetching}
+              />
+            }
+          />
+          <Route
+            path="/blog-manager"
+            element={
+              <BlogManager
+                adminBlogList={adminBlogList.message}
+                deleteBlog={deleteBlog}
+                fetchSingleBlog={fetchSingleBlog}
+                urlEndpoint={urlEndpoint}
+                setAdminBlogsLoading={setAdminBlogsLoading}
               />
             }
           />
